f8f86037949a15eac85f5428601672d73490ccdb
Umitosan <yeahdom@gmail.com>
2018-05-02 21:32:05 -0700

    add skeleton

---
+++ app.js
@@ -0,0 +1,9 @@
+/* jshint esversion: 6 */
+
+
+
+
+// FRONT END
+$(document).ready( function() {
+
+});
---
+++ scripts/logo/logo_db1.js
@@ -0,0 +1,48 @@
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'blue';
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.moveTo(10,10);
+ctx.lineTo(10,110);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(10,60,50,-(Math.PI/2),(Math.PI/2));
+ctx.stroke();
+ctx.fill();
+
+ctx.strokeStyle = 'green';
+ctx.fillStyle = 'green';
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.moveTo(62,55);
+ctx.lineTo(62,110);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(62,75,20,-(Math.PI/2),(Math.PI/2));
+ctx.stroke();
+ctx.fill();
+
+ctx.strokeStyle = 'red';
+ctx.fillStyle = 'red';
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.moveTo(84,72);
+ctx.lineTo(84,110);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(84,82,9,-(Math.PI/2),(Math.PI/2));
+ctx.stroke();
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(84,100,9,-(Math.PI/2),(Math.PI/2));
+ctx.stroke();
+ctx.fill();
+
+// context.arc(x,y,r,sAngle,eAngle,counterclockwise);
